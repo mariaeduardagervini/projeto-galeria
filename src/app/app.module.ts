917f82966013af5c2a7d336538c76c5c099f7aba
Maria Eduarda <mariaeduardagervini@hotmail.com>
2023-02-24 17:58:23 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GaleriaComponent } from '../components/galeria/galeria.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, GaleriaComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/components/galeria/galeria.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-galeria',
+  templateUrl: './galeria.component.html',
+  styleUrls: ['./galeria.component.css'],
+})
+export class GaleriaComponent {
+  @Input() public titulo: string = '';
+  @Input() public fotos: string[] = [];
+
+  public fotoAtual = 0;
+
+  public voltarParaPrimeira() {
+    this.fotoAtual = this.primeira();
+  }
+
+  public vaiParaProxima() {
+    this.fotoAtual++;
+  }
+
+  public vaiParaUltima() {
+    this.fotoAtual = this.ultima();
+  }
+
+  public voltarParaAnterior() {
+    this.fotoAtual--;
+  }
+
+  public primeira() {
+    return 0;
+  }
+
+  public ultima() {
+    return this.fotos.length - 1;
+  }
+}
